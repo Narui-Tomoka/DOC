8c633a844c135d3ddbc384f25ed1aa6bc8316197
Tomoka Narui <sweetmelty2512@gmail.com>
2026-07-31 07:49:27 +0900

    公開用にvite.config.js等を修正

--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,25 @@ import path from "path";
 
 export default defineConfig({
   root: ".", // index.html があるディレクトリ
-  base: "./", // 相対パス（ローカル動作用）
+  base: "/doc/",
   server: {
     open: true,
   },
   build: {
     outDir: "dist",
     assetsDir: "assets", // ← assets フォルダを固定名にしておくとパスが安定する！
+    emptyOutDir: true,
     rollupOptions: {
       input: {
         index: path.resolve(__dirname, "index.html"),
+        company: path.resolve(__dirname, "company.html"),
+        contact: path.resolve(__dirname, "contact.html"),
+        news: path.resolve(__dirname, "news.html"),
+        newsPage2: path.resolve(__dirname, "news-page2.html"),
+        newsPage3: path.resolve(__dirname, "news-page3.html"),
+        recruit: path.resolve(__dirname, "recruit.html"),
         service: path.resolve(__dirname, "service.html"),
+        works: path.resolve(__dirname, "works.html"),
       },
       output: {
         assetFileNames: "assets/[name]-[hash][extname]", // CSSや画像のパスを一定に
